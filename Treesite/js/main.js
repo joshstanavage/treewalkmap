ad3151d0ce42c9dd201d156dd2675568514b523e
joshstanavage <joshuastanavage@Joshuas-MacBook-Pro.local>
2021-03-11 10:46:56 -0500

    general updates

--- Treesite/js/main.js
+++ Treesite/js/main.js
@@ -27,11 +27,11 @@ accordionBtns.forEach((accordion) => {
   };
 });
 
-var mymap = L.map('mapid').setView([41.809142, -72.2573872], 15.91);
-L.tileLayer('https://api.mapbox.com/styles/v1/jstanavage/ckgfk17wv86e419qti0wfv98l.html?fresh=true&title=copy&access_token=pk.eyJ1IjoianN0YW5hdmFnZSIsImEiOiJja2dmanh5bHIwc2g4MnFwZHp2eDJybTB1In0.hVi2olDE2ekOv_8Vq3L4cw',  {
+var mymap = L.map('mapid').setView([72.2573872, 41.808 ], 15.91);
+L.tileLayer('https://api.mapbox.com/styles/v1/jstanavage/ckgfk17wv86e419qti0wfv98l.html?fresh=true&title=view&access_token=pk.eyJ1IjoianN0YW5hdmFnZSIsImEiOiJja2dmanh5bHIwc2g4MnFwZHp2eDJybTB1In0.hVi2olDE2ekOv_8Vq3L4cw',  {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v9',
+    id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoianN0YW5hdmFnZSIsImEiOiJja2dmanh5bHIwc2g4MnFwZHp2eDJybTB1In0.hVi2olDE2ekOv_8Vq3L4cw'
